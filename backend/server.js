4f00e9fd7455cd4cbd69d6246b85639b238188ea
Palak Agnihotri <agnihotripalak15@gmail.com>
2024-09-19 10:14:35 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -36,14 +36,7 @@ if (process.env.NODE_ENV === "production") {
 // --------------------------deployment------------------------------
 app.use(notFound);
 app.use(errorHandler);
-// app.get("/api/chat", (req, res) => {
-//   res.send(chats);
-// });
-// app.get("/api/chat/:id", (req, res) => {
-//   //console.log(req.params.id);
-//   const singleChat = chats.find((c) => c._id === req.params.id);
-//   res.send(singleChat);
-// });
+
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
   5000,
