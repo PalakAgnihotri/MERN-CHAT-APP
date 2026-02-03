d245640cc5f5a431bd01d735744b5bb93e853a62
Palak Agnihotri <143431220+PalakAgnihotri@users.noreply.github.com>
2026-02-03 16:59:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,15 @@ app.use(express.json()); // to accept json data
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../frontend/build", "index.html")
+    );
+  });
+}
 
 // Error Handling middlewares
 app.use(notFound);
@@ -71,12 +80,3 @@ io.on("connection", (socket) => {
     socket.leave(userData._id);
   });
 });
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "../frontend/build", "index.html")
-    );
-  });
-}
